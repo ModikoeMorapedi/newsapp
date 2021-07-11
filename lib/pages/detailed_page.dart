768feb304571edd602352305c655b0b4e937e31d
morapem <morapem@telkom.co.za>
2021-07-11 17:27:51 +0200

    added text on the App Bar widget

--- lib/pages/detailed_page.dart
+++ lib/pages/detailed_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:newsapp/pages/base_page/base_view.dart';
 import 'package:newsapp/scoped_model/home_scoped_model.dart';
 import 'package:newsapp/widgets/busy_indicator/busy_overlay_widget.dart';
@@ -14,7 +15,14 @@ class _DetailedPageState extends State<DetailedPage> {
   @override
   Widget build(BuildContext context) {
     return BaseView<HomeScopedModel>(
-      builder: (context, child, model) => BusyOverlayWidget(),
+      builder: (context, child, model) => BusyOverlayWidget(
+          child: Scaffold(
+        body: Container(
+          color: Colors.blue,
+          width: 200,
+          height: 200,
+        ),
+      )),
     );
   }
 }
